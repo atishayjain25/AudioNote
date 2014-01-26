94e759f3b9e7183c10df557c95cdeb0fe78ec2bc
Abhijeet <abhijeetsinha027@gmail.com>
2014-01-26 16:14:56 +0530

    list view implementtaion

---
+++ src/com/android/audionote/AudioNote_main_activity.java
@@ -0,0 +1,52 @@
+package com.android.audionote;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+//import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+//import android.util.Log;
+import android.view.View;
+//import android.widget.AdapterView;
+import android.widget.ListView;
+//import android.widget.TextView;
+//import android.widget.Toast;
+//import android.widget.AdapterView.OnItemClickListener;
+
+public class AudioNote_main_activity extends ListActivity {
+
+	static final String[] MOBILE_OS = 
+          new String[] { "Android", "iOS", "WindowsMobile", "Blackberry"};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setListAdapter(new MobileArrayAdapter(this, MOBILE_OS));
+		//setContentView(R.layout.activity_main);
+	}
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+
+		//get selected items
+		String selectedValue = (String) getListAdapter().getItem(position);
+		//Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
+		Intent i = new Intent(getApplicationContext(), SingleListItem.class);
+      // sending data to new activity
+      i.putExtra("product", selectedValue);
+      System.out.println("*************"+selectedValue);
+      startActivity(i);
+
+	}
+	
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.audio_note_main_activity, menu);
+		return true;
+	}
+
+}
+
